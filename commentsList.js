a2c912c6b3dcb6ca0ddd8bd7071246c03cab3ca2
tr4m0ryp - AppelMoussa <tramoryp@proton.me>
2025-02-07 19:29:31 +0100

    Create commentsList.js

---
+++ commentsList.js
@@ -0,0 +1,10 @@
+const comments = [
+    "Don't miss out! Join our AI-powered crypto revolution now!",
+    "test"
+];
+
+function getRandomComment() {
+    return comments[Math.floor(Math.random() * comments.length)];
+}
+
+module.exports = getRandomComment;
