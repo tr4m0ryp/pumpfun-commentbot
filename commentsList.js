45dec3ad3cf99b86acc52176ef76871c42e47f29
tr4m0ryp - AppelMoussa <tramoryp@proton.me>
2025-02-07 19:43:13 +0100

    Update commentsList.js

--- commentsList.js
+++ commentsList.js
@@ -7,4 +7,9 @@ function getRandomComment() {
     return comments[Math.floor(Math.random() * comments.length)];
 }
 
-module.exports = getRandomComment;
+// Detecteer of we CommonJS of ESM gebruiken
+if (typeof module !== "undefined" && module.exports) {
+    module.exports = getRandomComment;  // Voor CommonJS (require)
+} else {
+    export default getRandomComment;  // Voor ES Modules (import)
+}
